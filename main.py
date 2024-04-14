dd9b1635d70fbd87a02b56f835d9acaf139fa41d
Lucas Silva <lucasvitorsilva17@gmail.com>
2024-04-13 21:22:34 -0300

    adicionado o arquivo main.py

---
+++ main.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
+from prophet import Prophet
+
+# Baixar dados dos último quatro ano para uma ação específica
+dados = yf.download("JNJ", start="2020-01-01",
+                    end="2023-12-31", progress=False)
+
+dados = dados.reset_index()
+# Vamos dividir os dados em treino (até o final do primeiro semestre de 2023) e teste (segundo semestre de 2023)
+dados_treino = dados[dados['Date'] < '2023-07-31']
+dados_teste = dados[dados['Date'] >= '2023-07-31']
+
+# Preparando os dados para o FBProphet
+dados_prophet_treino = dados_treino[['Date', 'Close']].rename(
+    columns={'Date': 'ds', 'Close': 'y'})
+
+# Criar e treinar o modelo
+modelo = Prophet(weekly_seasonality=True,
+                 yearly_seasonality=True,
+                 daily_seasonality=False)
+
+modelo.add_country_holidays(country_name='US')
+
+modelo.fit(dados_prophet_treino)
+
+# Criar datas futuras para previsão até o final de 2023
+futuro = modelo.make_future_dataframe(periods=150)
+previsao = modelo.predict(futuro)
+
+# Plotar os dados de treino, teste e previsões
+plt.figure(figsize=(14, 8))
+plt.plot(dados_treino['Date'], dados_treino['Close'],
+         label='Dados de Treino', color='blue')
+plt.plot(dados_teste['Date'], dados_teste['Close'],
+         label='Dados Reais (Teste)', color='green')
+plt.plot(previsao['ds'], previsao['yhat'],
+         label='Previsão', color='orange', linestyle='--')
+
+plt.axvline(dados_treino['Date'].max(), color='red',
+            linestyle='--', label='Início da Previsão')
+plt.xlabel('Data')
+plt.ylabel('Preço do Fechamento')
+plt.title('Previsão de Preço de Fechamento vs Dados Reais')
+plt.legend()
+plt.show()
